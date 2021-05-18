3bf0673a8dfd13e71c89411322837bd03a98fcd1
HADES-01 <adityavats122017@gmail.com>
2021-05-18 15:02:36 +0530

    Added Local Storage Functionality

--- src/JokeList.js
+++ src/JokeList.js
@@ -15,8 +15,7 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
     };
-    this.handleVote = this.handleVote.bind(this);
-    this.getJokes = this.getJokes.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -35,8 +34,14 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
-    this.setState({ jokes: jokes });
+    this.setState(st => ({ 
+        jokes: [...st.jokes, ...jokes] 
+    }));
     window.localStorage.setItem("jokes", JSON.stringify(jokes));
+  }
+
+  handleClick(){
+      this.getJokes(); 
   }
 
   handleVote(id, delta) {
@@ -44,7 +49,7 @@ class JokeList extends Component {
       jokes: st.jokes.map((j) =>
         j.id === id ? { ...j, votes: j.votes + delta } : j
       ),
-    }));
+    }), () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)));
   }
 
   render() {
@@ -58,7 +63,9 @@ class JokeList extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="emoji"
           />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map((joke) => (
